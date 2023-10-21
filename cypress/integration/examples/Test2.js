ee9ad646b38c40be7c0fc30c5a7f443993826ff3
Ala123Sobhan <ala123sobhan@gmail.com>
2023-10-20 21:55:32 -0500

    add async cypress tests with details

--- cypress/integration/examples/Test2.js
+++ cypress/integration/examples/Test2.js
@@ -1,42 +1,25 @@
 /// <reference types="Cypress" />
 
 describe("first test suite", function () {
-  it("My first test case", function () {
+  it("My second test case", function () {
     cy.visit("https://rahulshettyacademy.com/seleniumPractise/#/");
     cy.get(".search-keyword").type("ca");
     cy.wait(1000);
-    cy.get(".product:visible").should("have.length", 4);
-
     //create alias
     cy.get(".products").as("productLocator");
-    //parent child chaning
-    cy.get("@productLocator").find(".product").should("have.length", 4);
-
-    //get 1st indext
-    cy.get("@productLocator")
-      .find(".product")
-      .eq(1)
-      .contains("ADD TO CART")
-      .click()
-      .then(function () {
-        console.log("sequential log because its wrapped");
-      });
-
+    var vegText = "";
     cy.get("@productLocator")
       .find(".product")
       .each(($e1, index, $list) => {
-        const vegText = $e1.find("h4.product-name").text();
+        vegText = $e1.find("h4.product-name").text();
         if (vegText.includes("Cashews")) {
           cy.wrap($e1).find("button").click();
         }
       });
-    cy.get(".brand").then(function (logoelement) {
-      cy.log(logoelement.text());
-    });
-
-    console.log("log in browser console");
-
-    cy.get(".brand").should("have.text", "GREENKART");
+    cy.get(".tada").click();
+    cy.contains("PROCEED TO CHECKOUT").click();
+    cy.get(".product-name").should("include.text", vegText);
+    cy.contains("Place Order").click();
   });
 });
 
