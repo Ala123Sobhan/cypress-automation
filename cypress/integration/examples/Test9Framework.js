62c21349e7873f8c8825f4b4b59f47b5c806c15c
Ala123Sobhan <ala123sobhan@gmail.com>
2023-10-22 10:39:28 -0500

    cypress hook & fixture for data driven tests

---
+++ cypress/integration/examples/Test9Framework.js
@@ -0,0 +1,29 @@
+/// <reference types="Cypress" />
+/// <reference types ="cypress-iframe"/>
+import "cypress-iframe";
+
+describe("second test suite", function () {
+  //run before all test once - before hook
+  before(function () {
+    cy.fixture("example").then(function (data) {
+      this.data = data;
+    });
+  });
+
+  it("My 2nd suite 1st test case", function () {
+    cy.visit("https://rahulshettyacademy.com/angularpractice/");
+    cy.get('input[name = "name"]:nth-child(2)').type(this.data.name);
+
+    cy.get("#exampleFormControlSelect1").select(this.data.gender);
+  });
+});
+
+//node_modules/.bin/cypress open - open runner
+//./node_modules/.bin/cypress run to run from cli & in headless mode
+// add --headed for visibility
+// add --browser chrome/firefox/electron/edge
+//cypress supports css selectors only - tagname#id, tagname.classname, tagname[attr = 'value'], tagname childtagname
+//cypress is async like js, every commands returns a promise which handled internally by cypress
+//non cypress command like text() cant resolve promise by themselves, need to be handled
+
+//for iframe - install - npm install -D cypress-iframe
