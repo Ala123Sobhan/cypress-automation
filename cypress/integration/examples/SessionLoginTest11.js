5ea8b60c0d3f85dee3d4afb3c045f3edc19f9172
Ala123Sobhan <ala123sobhan@gmail.com>
2023-10-24 21:56:13 -0500

    parse csv test

--- cypress/integration/examples/SessionLoginTest11.js
+++ cypress/integration/examples/SessionLoginTest11.js
@@ -1,13 +1,53 @@
 /// <reference types="Cypress" />
-
+const neatCSV = require("neat-csv");
+let productName;
 describe("second test suite", function () {
-  it("JWT Session Test", function () {
+  it("JWT Session Test", async () => {
     cy.LoginAPI().then(function () {
       cy.visit("https://rahulshettyacademy.com/client", {
         onBeforeLoad: function (window) {
           window.localStorage.setItem("token", Cypress.env("token"));
         },
       });
+    });
+
+    cy.get(".card-body b")
+      .eq(1)
+      .then(function (ele) {
+        productName = ele.text();
+      });
+
+    cy.get(".card-body button:last-of-type").eq(1).click();
+
+    cy.get("[routerlink*='cart']").click({ force: "click" });
+
+    cy.contains("Checkout").click();
+
+    cy.get("[placeholder*='Country']").type("ind");
+
+    cy.get(".ta-results button").each(($e1, index, $list) => {
+      if ($e1.text() === " India") {
+        cy.wrap($e1).click();
+      }
+    });
+
+    cy.get(".action__submit").click();
+
+    cy.wait(2000);
+
+    cy.get(".order-summary button").eq(0).click();
+
+    cy.readFile(
+      Cypress.config("fileServerFolder") +
+        "/cypress/downloads/order-invoice_ala123sobhan.csv"
+    ).then(async (text) => {
+      const csv = await neatCSV(text);
+
+      console.log(csv);
+
+      const actualProductCSV = csv[0]["Product Name"];
+
+      expect(productName).to.equal(actualProductCSV);
     });
   });
 });
